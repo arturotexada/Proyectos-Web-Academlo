7d54babca74babee8da0493e75dd1010f1b696b8
Arturo Texada <arturo.tejada@gmail.com>
2023-05-09 20:55:52 -0600

    https://github.com/arturotexada/Proyectos-Web-Academlo

---
+++ src/app.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const db = require('./utils/database');
+const Users = require("./models/users.models")
+require('./models/users.models');
+//const Countries = require('./models/countries.models');
+//const { log } = require('console');
+// creamos una instancia de express llamada app
+
+db.authenticate() // es una función asíncrona
+    .then(() => console.log('Base de datos conectada'))
+    .catch((err) => console.log(err));
+
+db.sync()
+    .then(() => console.log("Base sincronizada"))
+    .catch((error) => console.log(error));
+
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Servidor corriendo...')
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const newUser = req.body;
+        // INSERT INTO users (firstname, lastname, email, password) VALUES ()   
+        await Users.create(newUser);
+        res.status(201).send();
+        /*         firstname, lastname, email, password   */
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+// obtener a todos los usuarios de la base d edatos
+// users.findAll();
+
+/* app.get('/users', async (req, res) => {
+    try {
+        const users = await Users.findAll({
+            attributes: ['firstname', 'lastname', 'email']
+        });
+        res.json(users);
+    } catch (error) {
+        res.status(400).json(error);
+
+    }
+}) */
+
+/* app.get('/users/:id', async (req, res) => {
+    try {
+        // para recuperar el parámetro ID
+        //  * request param
+        //  es un objeto  {id: 23}
+        const { id } = req.params;
+        console.log(req.params)
+
+        const user = await Users.findByPk(id, {
+            attributes: {
+                exclude: ["passwords"],
+            },
+        });
+        res.json(user);
+    } catch (error) {
+        res.status(400).json(error);
+
+    }
+}); */
+
+app.get('/users/email/:email', async (req, res) => {
+    try {
+        // para recuperar el parámetro ID
+        //  * request param
+        //  es un objeto  {id: 23}
+        const { email } = req.params;
+        const user = await Users.findOne({
+            where: { email },
+        });
+        res.json(user);
+    } catch (error) {
+        res.status(400).json(error);
+
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Users.destroy({
+            where: { id }  // where:  {id : id}
+        });
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { firstname, lastname } = req.body;
+        // 1.actualizan nombre
+        // 2.actualizan apellido
+        // 3.actualizan los dos
+        await Users.update({ firstname, lastname }, {
+            where: { id }
+        });
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.listen(8000, () => {
+    console.log("Servidor escuchando en puerto 8000")
+});
+
+console.log(process.env);
---
+++ src/utils/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+// Vamos a gestionar la conexión con una base de datos
+// Importat sequelize
+const { Sequelize } = require('sequelize');
+
+// Crear una instancia de sequelize con la configuración
+// de conexión
+
+const db = new Sequelize({
+    host: process.env.DB_HOST, // "dpg-chdfag3hp8u3v707v0d0-a.ohio-postgres.render.com",
+    database: process.env.DB_NAME, //"users_crud_775s",
+    port: process.env.DB_PORT, //5432,
+    username: process.env.DB_USERNAME, //'arturoxts',
+    password: process.env.DB_PASSWORD, //'pS2KgX4pq0BrP5GWFqdil1PDZCoW9fRP',
+    dialect: 'postgres',
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
+});
+
+module.exports = db;
+
+//  postgres://arturoxts:pS2KgX4pq0BrP5GWFqdil1PDZCoW9fRP@dpg-chdfag3hp8u3v707v0d0-a.ohio-postgres.render.com/users_crud_775s
